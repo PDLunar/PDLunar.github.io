9e156b0836a06c54da5cef9b205611872891b4a3
PDLunar <ljyun0908@gmail.com>
2025-12-15 12:19:12 +0900

    Update custom.js

--- docs/assets/js/custom.js
+++ docs/assets/js/custom.js
@@ -1,1 +1,8 @@
+document.addEventListener("DOMContentLoaded", () => {
+  const toggle = document.getElementById("dark-toggle");
+  if (!toggle) return;
 
+  toggle.addEventListener("click", () => {
+    document.body.classList.toggle("dark-mode");
+  });
+});
